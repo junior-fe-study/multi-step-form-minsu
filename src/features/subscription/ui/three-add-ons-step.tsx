609ebe105f5eb066df4335113350860254ad0851
Minsu <alstntorl@naver.com>
2025-03-09 16:08:53 +0900

    feat: 구독 Funnel 각각의 Step들 use-funnel 비즈니스 로직 추가

--- src/features/subscription/ui/three-add-ons-step.tsx
+++ src/features/subscription/ui/three-add-ons-step.tsx
@@ -1,16 +1,17 @@
 import { cn } from "@/shared/cn";
 import { SubscriptionLayout } from "@/widgets/subscription-layout";
-import { useState } from "react";
 import { Iterator } from "@modern-kit/react";
 
-type AddOn = {
-  id: number;
+type PlanType = "arcade" | "advanced" | "pro";
+
+type Addon = {
+  id: 1 | 2 | 3;
   title: string;
   description: string;
   price: number;
 };
 
-const addOns: AddOn[] = [
+const ADDONS: Addon[] = [
   {
     id: 1,
     title: "Online service",
@@ -32,32 +33,21 @@ const addOns: AddOn[] = [
 ];
 
 export const ThreeAddOnsStep = ({
-  이전_스탭_이동,
-  다음_스탭_이동,
+  plan,
+  addons,
+  handleAddonToggle,
+  prevStep,
+  nextStep,
 }: {
-  이전_스탭_이동: () => void;
-  다음_스탭_이동: () => void;
-}) => {
-  const [addons, setAddons] = useState<number[]>([]);
-
-  const [plan, setPlan] = useState<{
-    type: string;
+  plan: {
+    type: PlanType;
     isYearly: boolean;
-  }>({
-    type: "arcade",
-    isYearly: false,
-  });
-
-  const handleAddonToggle = (addonId: number) => {
-    setAddons((prev) => {
-      if (prev.includes(addonId)) {
-        return prev.filter((id) => id !== addonId);
-      } else {
-        return [...prev, addonId];
-      }
-    });
   };
-
+  addons: Addon[];
+  handleAddonToggle: (addon: Addon) => void;
+  prevStep: () => void;
+  nextStep: () => void;
+}) => {
   return (
     <SubscriptionLayout
       현재_단계={3}
@@ -75,52 +65,55 @@ export const ThreeAddOnsStep = ({
           <div className="mt-[35px] w-[450px] space-y-4">
             <Iterator
               itemKey={"id"}
-              items={addOns}
-              renderItem={(addon) => (
-                <div
-                  key={addon.id}
-                  className={cn(
-                    "flex cursor-pointer items-center gap-4 rounded-lg border p-4 hover:border-[#6259FF]",
-                    {
-                      "border-[#6259FF] bg-[#F8F9FF]": addons.includes(
-                        addon.id,
-                      ),
-                      "border-gray-200": !addons.includes(addon.id),
-                    },
-                  )}
-                  onClick={() => handleAddonToggle(addon.id)}
-                >
-                  <input
-                    type="checkbox"
-                    checked={addons.includes(addon.id)}
-                    onChange={() => handleAddonToggle(addon.id)}
-                    className="h-5 w-5 rounded border-gray-300 text-[#6259FF] focus:ring-[#6259FF]"
-                  />
-                  <div className="flex-grow">
-                    <h3 className="font-medium text-[#022959]">
-                      {addon.title}
-                    </h3>
-                    <p className="text-sm text-gray-500">{addon.description}</p>
+              items={ADDONS}
+              renderItem={(addon) => {
+                const isSelected = addons.some((a) => a.id === addon.id);
+                return (
+                  <div
+                    key={addon.id}
+                    className={cn(
+                      "flex cursor-pointer items-center gap-4 rounded-lg border p-4 hover:border-[#6259FF]",
+                      {
+                        "border-[#6259FF] bg-[#F8F9FF]": isSelected,
+                        "border-gray-200": !isSelected,
+                      },
+                    )}
+                    onClick={() => handleAddonToggle(addon)}
+                  >
+                    <input
+                      type="checkbox"
+                      checked={isSelected}
+                      onChange={() => handleAddonToggle(addon)}
+                      className="h-5 w-5 rounded border-gray-300 text-[#6259FF] focus:ring-[#6259FF]"
+                    />
+                    <div className="flex-grow">
+                      <h3 className="font-medium text-[#022959]">
+                        {addon.title}
+                      </h3>
+                      <p className="text-sm text-gray-500">
+                        {addon.description}
+                      </p>
+                    </div>
+                    <p className="text-sm text-[#6259FF]">
+                      +${plan.isYearly ? addon.price * 10 : addon.price}/
+                      {plan.isYearly ? "yr" : "mo"}
+                    </p>
                   </div>
-                  <p className="text-sm text-[#6259FF]">
-                    +${plan.isYearly ? addon.price * 10 : addon.price}/
-                    {plan.isYearly ? "yr" : "mo"}
-                  </p>
-                </div>
-              )}
+                );
+              }}
             />
           </div>
 
           {/* Prev / Next Step 버튼 영역 */}
           <div className="mt-[24px] flex justify-between">
             <button
-              onClick={이전_스탭_이동}
+              onClick={prevStep}
               className="h-[48px] w-[123px] cursor-pointer rounded-lg text-[#9699AA]"
             >
               Go Back
             </button>
             <button
-              onClick={다음_스탭_이동}
+              onClick={nextStep}
               className="h-[48px] w-[123px] cursor-pointer rounded-lg bg-[#022959] text-[#fff]"
             >
               Next Step
--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -1,3 +1,201 @@
+"use client";
+
+import {
+  FourSummaryStep,
+  OneYourInfoStep,
+  ResultThankYouStep,
+  ThreeAddOnsStep,
+  TwoSelectPlanStep,
+} from "@/features/subscription";
+import { createFunnelSteps, useFunnel } from "@use-funnel/browser";
+
+type Addon = {
+  id: 1 | 2 | 3;
+  title: string;
+  description: string;
+  price: number;
+};
+
+interface SubscriptionFunnelContext {
+  name: string;
+  email: string;
+  phone: string;
+  plan: {
+    type: "arcade" | "advanced" | "pro";
+    isYearly: boolean;
+  };
+  addons: Addon[];
+}
+
 export default function HomePage() {
-  return <></>;
+  const steps = createFunnelSteps<Partial<SubscriptionFunnelContext>>()
+    .extends("YourInfo", { requiredKeys: ["name", "email", "phone"] })
+    .extends("SelectPlan", { requiredKeys: ["plan"] })
+    .extends("AddOns", { requiredKeys: ["addons"] })
+    .extends("Summary")
+    .extends("ThankYou")
+    .build();
+
+  const subscribe = useFunnel({
+    id: "subscription-funnel",
+    steps,
+    initial: {
+      step: "YourInfo",
+      context: {
+        name: "",
+        email: "",
+        phone: "",
+        plan: {
+          type: "arcade",
+          isYearly: false,
+        },
+        addons: [],
+      },
+    },
+  });
+  console.log(subscribe.context);
+  return (
+    <subscribe.Render
+      YourInfo={({ history, context }) => (
+        <OneYourInfoStep
+          name={context.name}
+          email={context.email}
+          phone={context.phone}
+          handleNameChange={(e) =>
+            history.push("YourInfo", { ...context, name: e.target.value })
+          }
+          handleEmailChange={(e) =>
+            history.push("YourInfo", { ...context, email: e.target.value })
+          }
+          handlePhoneNumberChange={(e) =>
+            history.push("YourInfo", { ...context, phone: e.target.value })
+          }
+          nextStep={() =>
+            history.push("SelectPlan", {
+              ...context,
+              plan: {
+                type: "arcade",
+                isYearly: false,
+              },
+            })
+          }
+        />
+      )}
+      SelectPlan={({ history, context }) => (
+        <TwoSelectPlanStep
+          plan={context.plan!}
+          handlePlanSelection={(
+            selectedPlanType: "arcade" | "advanced" | "pro",
+          ) =>
+            history.push("SelectPlan", {
+              ...context,
+              plan: { ...context.plan, type: selectedPlanType },
+            })
+          }
+          toggleBillingCycle={() =>
+            history.push("SelectPlan", {
+              ...context,
+              plan: { ...context.plan, isYearly: !context.plan.isYearly },
+            })
+          }
+          prevStep={() =>
+            history.push("YourInfo", {
+              name: context.name,
+              email: context.email,
+              phone: context.phone,
+            })
+          }
+          nextStep={() =>
+            history.push("AddOns", {
+              ...context,
+              addons: [],
+            })
+          }
+        />
+      )}
+      AddOns={({ history, context }) => (
+        <ThreeAddOnsStep
+          plan={context.plan}
+          addons={context.addons || []}
+          handleAddonToggle={(addon) => {
+            const currentAddons = context.addons || [];
+            const newAddons = currentAddons.some((a) => a.id === addon.id)
+              ? currentAddons.filter((a) => a.id !== addon.id)
+              : [...currentAddons, addon];
+
+            history.push("AddOns", {
+              ...context,
+              addons: newAddons,
+            });
+          }}
+          prevStep={() =>
+            history.push("SelectPlan", {
+              name: context.name,
+              email: context.email,
+              phone: context.phone,
+              plan: context.plan,
+            })
+          }
+          nextStep={() => history.push("Summary", context)}
+        />
+      )}
+      Summary={({ history, context }) => (
+        <FourSummaryStep
+          plan={context.plan}
+          addons={context.addons}
+          planPrice={(() => {
+            const planPrices = {
+              arcade: 9,
+              advanced: 12,
+              pro: 15,
+            };
+
+            // 기본 가격 계산
+            const basePrice = planPrices[context.plan.type];
+
+            // 연간/월간 구독에 따른 가격 조정
+            return basePrice * (context.plan.isYearly ? 10 : 1);
+          })()}
+          total={(() => {
+            const planPrices = {
+              arcade: 9,
+              advanced: 12,
+              pro: 15,
+            };
+
+            // 기본 플랜 가격 계산
+            const basePrice = planPrices[context.plan.type];
+            const planPrice = basePrice * (context.plan.isYearly ? 10 : 1);
+
+            const addonsTotal = context.addons.reduce((sum, addon) => {
+              return sum + addon.price * (context.plan.isYearly ? 10 : 1);
+            }, 0);
+
+            // 총액 계산
+            return planPrice + addonsTotal;
+          })()}
+          handleChangePlan={() =>
+            history.push("SelectPlan", {
+              name: context.name,
+              email: context.email,
+              phone: context.phone,
+              plan: context.plan,
+              addons: context.addons,
+            })
+          }
+          prevStep={() =>
+            history.push("AddOns", {
+              name: context.name,
+              email: context.email,
+              phone: context.phone,
+              plan: context.plan,
+              addons: context.addons,
+            })
+          }
+          nextStep={() => history.push("ThankYou", context)}
+        />
+      )}
+      ThankYou={() => <ResultThankYouStep />}
+    />
+  );
 }
